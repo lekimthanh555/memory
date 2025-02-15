a4fdcbf59f2d796735dcee09c0f5eea1ba6c83e6
KimThanh <101157522+lekimthanh555@users.noreply.github.com>
2025-02-15 18:20:23 +0700

    dung dong ho
    // var timer = setInterval(function(){olock()}, 1000);

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function(){
             sec =  Math.floor((today - yourDate) / 1000) % 60;
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
-      var timer = setInterval(function(){olock()}, 1000);
+     // var timer = setInterval(function(){olock()}, 1000);
       document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
 
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
