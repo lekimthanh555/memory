3e5f01920e96c1f0a1543ab73c449787007b9b8f
KimThanh <101157522+lekimthanh555@users.noreply.github.com>
2025-02-15 18:32:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-function olock() {
-    var hrs = 11,  // Giờ cố định
-        min = 26,  // Phút cố định
-        sec = 30;  // Giây cố định
+document.addEventListener('DOMContentLoaded', function(){
+    var rootTime = document.querySelector("time");
+    var rootDate = document.querySelector("date");
 
-    rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
-}
+    // Hiển thị đúng ngày mong muốn (10 DAYS)
+    rootDate.textContent = "1069 DAYS";
+
+    function olock() {
+        var hrs = 11,  // Giờ cố định
+            min = 26,  // Phút cố định
+            sec = 30;  // Giây cố định
+
+        rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
+    } 
+    olock();
+});
